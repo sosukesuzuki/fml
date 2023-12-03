3a8d66bb6e15ff4e4399cb9e2fea98b68195273b
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-12-03 23:00:06 +0900

    Add more tests for compiler

--- fml_compiler_tests.c
+++ fml_compiler_tests.c
@@ -20,8 +20,82 @@ void compile_01()
     TEST_ASSERT(instructions.instructions[1]->kind, INSTRUCTION_MATCH);
 }
 
+void compile_02()
+{
+    Node node;
+    char* s = "ab";
+    parseRegexp(&node, s);
+
+    Instructions instructions;
+    initInstructions(&instructions);
+
+    int r = compile(&node, &instructions);
+
+    TEST_ASSERT(r, 0);
+    TEST_ASSERT(instructions.size, 3);
+    TEST_ASSERT(instructions.cap, 16);
+    TEST_ASSERT(instructions.instructions[0]->kind, INSTRUCTION_CHAR);
+    TEST_ASSERT(instructions.instructions[0]->u.iChar.c, 'a');
+    TEST_ASSERT(instructions.instructions[1]->kind, INSTRUCTION_CHAR);
+    TEST_ASSERT(instructions.instructions[1]->u.iChar.c, 'b');
+    TEST_ASSERT(instructions.instructions[2]->kind, INSTRUCTION_MATCH);
+}
+
+void compile_03()
+{
+    Node node;
+    char* s = "a*";
+    parseRegexp(&node, s);
+
+    Instructions instructions;
+    initInstructions(&instructions);
+
+    int r = compile(&node, &instructions);
+
+    TEST_ASSERT(r, 0);
+    TEST_ASSERT(instructions.size, 4);
+    TEST_ASSERT(instructions.cap, 16);
+    TEST_ASSERT(instructions.instructions[0]->kind, INSTRUCTION_SPLIT);
+    TEST_ASSERT(instructions.instructions[0]->u.iSplit.offset1, 1);
+    TEST_ASSERT(instructions.instructions[0]->u.iSplit.offset2, 3);
+    TEST_ASSERT(instructions.instructions[1]->kind, INSTRUCTION_CHAR);
+    TEST_ASSERT(instructions.instructions[1]->u.iChar.c, 'a');
+    TEST_ASSERT(instructions.instructions[2]->kind, INSTRUCTION_JMP);
+    TEST_ASSERT(instructions.instructions[2]->u.iJmp.offset, 0);
+    TEST_ASSERT(instructions.instructions[3]->kind, INSTRUCTION_MATCH);
+}
+
+void compile_04()
+{
+    Node node;
+    char* s = "a|b";
+    parseRegexp(&node, s);
+
+    Instructions instructions;
+    initInstructions(&instructions);
+
+    int r = compile(&node, &instructions);
+
+    TEST_ASSERT(r, 0);
+    TEST_ASSERT(instructions.size, 5);
+    TEST_ASSERT(instructions.cap, 16);
+    TEST_ASSERT(instructions.instructions[0]->kind, INSTRUCTION_SPLIT);
+    TEST_ASSERT(instructions.instructions[0]->u.iSplit.offset1, 1);
+    TEST_ASSERT(instructions.instructions[0]->u.iSplit.offset2, 3);
+    TEST_ASSERT(instructions.instructions[1]->kind, INSTRUCTION_CHAR);
+    TEST_ASSERT(instructions.instructions[1]->u.iChar.c, 'a');
+    TEST_ASSERT(instructions.instructions[2]->kind, INSTRUCTION_JMP);
+    TEST_ASSERT(instructions.instructions[2]->u.iJmp.offset, 4);
+    TEST_ASSERT(instructions.instructions[3]->kind, INSTRUCTION_CHAR);
+    TEST_ASSERT(instructions.instructions[3]->u.iChar.c, 'b');
+    TEST_ASSERT(instructions.instructions[4]->kind, INSTRUCTION_MATCH);
+}
+
 int main()
 {
     RUN_TEST(compile_01);
+    RUN_TEST(compile_02);
+    RUN_TEST(compile_03);
+    RUN_TEST(compile_04);
     return 0;
 }
