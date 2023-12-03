8e24ae970930f547de4c721b458ceaaf559dd6a3
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-12-03 23:19:59 +0900

    Implement vm stack

---
+++ fml_vm_stack.c
@@ -0,0 +1,40 @@
+#include "fml_vm_stack.h"
+
+#define VM_STACK_CAP 32
+
+void initStack(VMStack* stack)
+{
+    stack->top = -1;
+    stack->cap = VM_STACK_CAP;
+    stack->threads = malloc(sizeof(Thread) * stack->cap);
+}
+
+int isVMStackFull(VMStack* stack)
+{
+    return stack->top >= stack->cap - 1;
+}
+
+int pushVMStack(VMStack* stack, Thread* thread)
+{
+    if (isVMStackFull(stack)) {
+        return -1;
+    }
+    stack->top++;
+    stack->threads[stack->top] = thread;
+    return 0;
+}
+
+int isVMStackEmpty(VMStack* stack)
+{
+    return stack->top < 0;
+}
+
+Thread* popVMStack(VMStack* stack)
+{
+    if (isVMStackEmpty(stack)) {
+        return NULL;
+    }
+    Thread* thread = stack->threads[stack->top];
+    stack->top--;
+    return thread;
+}
---
+++ fml_vm_stack.h
@@ -0,0 +1,19 @@
+#ifndef FML_VM_STACK_H
+#define FML_VM_STACK_H
+
+typedef struct Thread {
+    int pc;
+    int sp;
+} Thread;
+
+typedef struct VMStack {
+    Thread** threads;
+    int top;
+    int cap;
+} VMStack;
+
+void initStack(VMStack* stack);
+int pushVMStack(VMStack* stack, Thread* thread);
+Thread* popVMStack(VMStack* stack);
+
+#endif
