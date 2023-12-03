f6945cd95e74d881df98a371fb7d2af4f5c4d5d8
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-12-03 21:52:40 +0900

    Add more tests for instructions

--- fml_instructions_tests.c
+++ fml_instructions_tests.c
@@ -1,7 +1,8 @@
 #include "fml_instructions.h"
 #include "fml_test_utils.h"
 
-void appendInstruction_01() {
+void appendInstruction_01()
+{
     Instructions instructions;
     initInstructions(&instructions);
 
@@ -14,7 +15,47 @@ void appendInstruction_01() {
     TEST_ASSERT(instructions.instructions[0].kind, INSTRUCTION_MATCH);
 }
 
-int main() {
+void appendInstruction_02()
+{
+    Instructions instructions;
+    initInstructions(&instructions);
+
+    Instruction instruction1;
+    instruction1.kind = INSTRUCTION_MATCH;
+    appendInstruction(&instructions, instruction1);
+
+    Instruction instruction2;
+    instruction2.kind = INSTRUCTION_CHAR;
+    instruction2.u.iChar.c = 'a';
+    appendInstruction(&instructions, instruction2);
+
+    TEST_ASSERT(instructions.size, 2);
+    TEST_ASSERT(instructions.cap, 16);
+    TEST_ASSERT(instructions.instructions[0].kind, INSTRUCTION_MATCH);
+    TEST_ASSERT(instructions.instructions[1].kind, INSTRUCTION_CHAR);
+}
+
+void appendInstruction_03()
+{
+    Instructions instructions;
+    initInstructions(&instructions);
+
+    int cap = 16;
+
+    for (int i = 0; i < cap + 1; i++) {
+        Instruction instruction;
+        instruction.kind = INSTRUCTION_MATCH;
+        appendInstruction(&instructions, instruction);
+    }
+
+    TEST_ASSERT(instructions.size, 17);
+    TEST_ASSERT(instructions.cap, cap * 2);
+}
+
+int main()
+{
     RUN_TEST(appendInstruction_01);
+    RUN_TEST(appendInstruction_02);
+    RUN_TEST(appendInstruction_03);
     return 0;
 }
