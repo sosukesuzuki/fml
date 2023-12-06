9372d44835ab555ce507b6a2f29171e932dab5cd
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-12-06 13:10:36 +0900

    Add tests

--- fml_lib_tests.c
+++ fml_lib_tests.c
@@ -11,9 +11,33 @@ void fmlTest_02()
     TEST_ASSERT(fmlTest("a", "b"), 0);
 }
 
+void fmlTest_03()
+{
+    TEST_ASSERT(fmlTest("a*", "aaaaaaaa"), 1);
+}
+
+void fmlTest_04()
+{
+    TEST_ASSERT(fmlTest("a|b", "a"), 1);
+}
+
+void fmlTest_05()
+{
+    TEST_ASSERT(fmlTest("a|b", "b"), 1);
+}
+
+// void fmlTest_06()
+// {
+//     TEST_ASSERT(fmlTest("a|b", "c"), 0);
+// }
+
 int main()
 {
     RUN_TEST(fmlTest_01);
     RUN_TEST(fmlTest_02);
+    RUN_TEST(fmlTest_03);
+    RUN_TEST(fmlTest_04);
+    RUN_TEST(fmlTest_05);
+    // RUN_TEST(fmlTest_06);
     return 0;
 }
